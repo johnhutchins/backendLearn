de83c7bab13046fd6a1a36237c6ce66d8e7f5b3c
John Hutchins <johnwallshutchins@gmail.com>
2020-01-20 10:30:36 -0500

    routing

--- build/server.js
+++ build/server.js
@@ -6,13 +6,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = express_1.default();
 app.get('/', (req, res, next) => {
-    res.send("John's Backend API, v1");
+    res.send("GET REQUEST\nJohn's Backend API, v1");
 });
 app.get('/tours', (req, res, next) => {
-    res.send("GET on the tours endpoint");
+    res.send("GET REQUEST for tours endpoint \nGET on the tours endpoint");
 });
 app.post('/tours', (req, res, next) => {
-    res.send("POST on the tours endpoint");
+    res.send("POST REQUEST FOR tours endpoint \nPOST on the tours endpoint");
 });
 app.listen(process.env.PORT || 8091, () => {
     console.log("Server started");
--- server.ts
+++ server.ts
@@ -2,15 +2,15 @@ import express from "express"
 const app = express()
 
 app.get('/',(req,res,next)=>{
-    res.send("John's Backend API, v1")
+    res.send("GET REQUEST\nJohn's Backend API, v1")
 })
 
 app.get('/tours', (req,res,next)=>{
-    res.send("GET on the tours endpoint")
+    res.send("GET REQUEST for tours endpoint \nGET on the tours endpoint")
 })
 
 app.post('/tours',(req,res,next)=>{
-    res.send("POST on the tours endpoint")
+    res.send("POST REQUEST FOR tours endpoint \nPOST on the tours endpoint")
 })
 
 app.listen(process.env.PORT || 8091, () =>{
