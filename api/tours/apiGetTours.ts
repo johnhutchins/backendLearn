4ed2b122b503e5d76d49b6c2dce444d549ec50f6
John Hutchins <johnwallshutchins@gmail.com>
2020-01-20 11:10:06 -0500

    routing progress

--- api/tours/apiGetTours.ts
+++ api/tours/apiGetTours.ts
@@ -1,6 +1,9 @@
 import { DataStore } from "../../data/data"
 import { RequestHandler } from "express"
+import { TourSummary } from "../../model/tourSummary"
 
 export const apiGetTours: RequestHandler = (req, res, next) =>{
-    res.json(DataStore.tours)
+    res.json(DataStore.tours.map((item: any) =>{
+        new TourSummary(item)
+    }))
 }
