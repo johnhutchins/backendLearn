ba85831c1972bbf2df47f30671cd5e0eb8286913
John Hutchins <johnwallshutchins@gmail.com>
2020-01-20 10:53:36 -0500

    progress

--- build/server.js
+++ build/server.js
@@ -6,12 +6,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const data_1 = require("./data/data");
 const app = express_1.default();
-console.log(JSON.stringify(data_1.DataStore.tours));
+console.log(JSON.parse(JSON.stringify(data_1.DataStore.tours)));
 app.get('/', (req, res, next) => {
     res.send("GET REQUEST\nJohn's Backend API, v1");
 });
 app.get('/tours', (req, res, next) => {
-    res.send("GET REQUEST for tours endpoint \nGET on the tours endpoint");
+    res.send(data_1.DataStore.tours);
 });
 app.post('/tours', (req, res, next) => {
     res.send("POST REQUEST FOR tours endpoint \nPOST on the tours endpoint");
--- server.ts
+++ server.ts
@@ -2,14 +2,14 @@ import express from "express"
 import { DataStore } from "./data/data"
 const app = express()
 
-console.log(JSON.stringify(DataStore.tours))
+console.log(JSON.parse(JSON.stringify(DataStore.tours)))
 
 app.get('/',(req,res,next)=>{
     res.send("GET REQUEST\nJohn's Backend API, v1")
 })
 
 app.get('/tours', (req,res,next)=>{
-    res.send("GET REQUEST for tours endpoint \nGET on the tours endpoint")
+    res.send(DataStore.tours)
 })
 
 app.post('/tours',(req,res,next)=>{
